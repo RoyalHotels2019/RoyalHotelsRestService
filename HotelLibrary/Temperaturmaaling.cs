2a9bec3acfa868973e8fb30c3f3cbd0a198443c6
thom20m1 <45196128+thom20m1@users.noreply.github.com>
2019-12-04 13:40:20 +0100

    added class library and controllers

---
+++ HotelLibrary/Temperaturmaaling.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelLibrary
+{
+    public class Temperaturmaaling
+    {
+        private int _hotelID;
+        private DateTime _datoTid;
+        private double _temperature;
+
+        public int HotelID
+        {
+            get { return _hotelID; }
+            set { _hotelID = value; }
+        }
+        public DateTime DatoTid
+        {
+            get { return _datoTid; }
+            set { _datoTid = value; }
+        }
+        public double Temperature
+        {
+            get { return _temperature; }
+            set { _temperature = value; }
+        }
+        public Temperaturmaaling()
+        {
+
+        }
+
+        public Temperaturmaaling(int hotelID, DateTime datoTid, double temperature)
+        {
+            _hotelID = hotelID;
+            _datoTid = datoTid;
+            _temperature = temperature;
+        }
+        public override string ToString()
+        {
+            return $"HotelID: {_hotelID}, DatoTid: {_datoTid}, Temperature: {_temperature}";
+        }
+    }
+}
--- RoyalHotelsRestService/DBUtil/HotelTempsManager.cs
+++ RoyalHotelsRestService/DBUtil/HotelTempsManager.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HotelLibrary;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,38 @@ namespace RoyalHotelsRestService.DBUtil
 {
     public class HotelTempsManager
     {
+            //thomas
+            private const string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ThomasTestRoyalHotels;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            //christian
+            //private const string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=RoyalHotel2019;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            //azure
+            //private const string ConnectionString = @"Server=tcp:thofoserver.database.windows.net,1433;Initial Catalog = thofodatabase; Persist Security Info=False;User ID = adminthofo; Password=grt45Lde; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;";
+
+
+
+            private const string INSERT = "INSERT INTO Hoteltemps (Tempe_Date, Hotel_Id, Tempe_Value) VALUES (@Tempe_Date, @Hotel_Id, @Tempe_Value)";
+
+
+            public bool Post(Temperaturmaaling maaling)
+            {
+                bool retValue;
+
+                SqlConnection connection = new SqlConnection(ConnectionString);
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(INSERT, connection);
+                cmd.Parameters.AddWithValue("@Tempe_Date", maaling.DatoTid);
+                cmd.Parameters.AddWithValue("@Hotel_Id", maaling.HotelID);
+                cmd.Parameters.AddWithValue("@Tempe_Value", maaling.Temperature);
+
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                retValue = rowsAffected == 1;
+                connection.Close();
+
+
+                return retValue;
+            }
     }
 }
