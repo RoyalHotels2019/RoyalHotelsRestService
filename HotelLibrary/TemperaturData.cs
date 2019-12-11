5e171cb56e1914b5a8466a95e2c760d507c4de2a
thom20m1 <45196128+thom20m1@users.noreply.github.com>
2019-12-11 14:37:26 +0100

    comitted changes

---
+++ HotelLibrary/TemperaturData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelLibrary
+{
+    public class TemperaturData
+    {
+        private int _hotelID;
+        private double _temperature;
+
+        public int HotelID
+        {
+            get { return _hotelID; }
+            set { _hotelID = value; }
+        }
+        public double Temperature
+        {
+            get { return _temperature; }
+            set { _temperature = value; }
+        }
+        public TemperaturData(int hotelID, double temperature)
+        {
+            _hotelID = hotelID;
+            _temperature = temperature;
+        }
+        public override string ToString()
+        {
+            return $"HotelID: {_hotelID},Temperature: {_temperature}";
+        }
+    }
+}
+
